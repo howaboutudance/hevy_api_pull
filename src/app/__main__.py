52aa5992e2940174351b682ca67ae6e547411d93
Michael <mike@hematite.tech>
2025-09-01 14:55:07 -0700

    implemented a pull all workouts and truncate & insert them into mongodb.

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,1 +1,19 @@
 """Main entry point for the application."""
+
+import asyncio
+import logging
+
+from app.app import pull_all_workouts
+
+_log = logging.getLogger(__name__)
+
+
+def main():
+    """Main execution function."""
+    _log.info("starting workout batch job")
+    asyncio.run(pull_all_workouts())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
--- src/app/conn.py
+++ src/app/conn.py
@@ -1,1 +1,29 @@
 """Database connection module."""
+
+import logging
+
+from pymongo import AsyncMongoClient
+
+from app.config import settings
+
+_log = logging.getLogger(__name__)
+
+
+class Mongo_Conn:
+    """MongoDB connection async context manager."""
+
+    # import ipdb; ipdb.set_trace()
+    _connection_str = f"mongodb://{settings.mongodb.username}:{settings.mongodb.password}@{settings.mongodb.host}"
+    _client = AsyncMongoClient(_connection_str)
+
+    def __init__(self, db_name: str):
+        """Initialize the database connection."""
+        self._db_name = db_name
+
+    async def __aenter__(self):
+        """Enter the database connection context."""
+        return self._client[self._db_name]
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """Exit the database connection context."""
+        await self._client.close()
