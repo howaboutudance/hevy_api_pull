4226a1d25e90114b177933453d88027e097a7ed6
Michael Penhallegon <mike@hematite.tech>
2025-10-12 15:25:27 -0700

    Changed to main entry point to a argparse command structure and added tests  for coverage

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,19 +1,48 @@
 """Main entry point for the application."""
 
+import argparse
 import asyncio
 import logging
 
+import uvicorn
+
 from app.app import pull_all_workouts
+from app.webhook import subscribe_app
 
 _log = logging.getLogger(__name__)
 
 
-async def main():
+def setup_parser() -> argparse.Namespace:
+    """Setup application parser."""
+    parser = argparse.ArgumentParser(description="Application Command Line Interface")
+    subparsers = parser.add_subparsers(dest="command")
+
+    serve_parser = subparsers.add_parser("serve", help="Start the webhook server")
+    serve_parser.add_argument("--host", type=str, default="0.0.0.0")
+    serve_parser.add_argument("--port", type=int, default=8000)
+
+    pull_parser = subparsers.add_parser("pull", help="Pull data from external source")
+    pull_parser.add_argument("--page_size", type=int, required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
     """Main execution function."""
-    _log.info("starting workout batch job")
-    await pull_all_workouts()
+    args = setup_parser()
+
+    match args.command:
+        case "serve":
+            uvicorn.run(subscribe_app, host=args.host, port=args.port)
+        case "pull":
+            _log.info("Pull command executed")
+            asyncio.run(pull_all_workouts(args.page_size))
+        case _:
+            _log.error("Unknown command")
+            raise SystemExit("Unknown command")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+    main()
--- test/test_main.py
+++ test/test_main.py
@@ -4,12 +4,37 @@ from unittest import mock
 
 import pytest
 
-from app.__main__ import main
+from app.__main__ import main, setup_parser
 
 
-@pytest.mark.asyncio
-async def test_main():
+def test_main_pull():
     """Test main function."""
-    with mock.patch("app.__main__.pull_all_workouts", mock.AsyncMock()) as m_pull:
-        await main()
-        m_pull.assert_called_once()
+    with mock.patch("app.__main__.setup_parser") as m_parser, mock.patch("app.__main__.pull_all_workouts") as m_pull:
+        m_parser.return_value = mock.Mock(command="pull", page_size=10)
+        main()
+        m_pull.assert_called_once_with(10)
+
+
+def test_main_serve():
+    """Test main function."""
+    with mock.patch("app.__main__.setup_parser") as m_parser, mock.patch("app.__main__.uvicorn.run") as m_serve:
+        m_parser.return_value = mock.Mock(command="serve", host="localhost", port=8000)
+        main()
+        m_serve.assert_called_once_with(mock.ANY, host="localhost", port=8000)
+
+
+def test_main_unknown():
+    """Test main function with unknown command."""
+    with mock.patch("app.__main__.setup_parser") as m_parser, pytest.raises(SystemExit):
+        m_parser.return_value = mock.Mock(command="unknown")
+        main()
+
+
+def test_setup_parser():
+    """Test setup_parser function."""
+    # push into the environment the args we want to test
+    test_args = ["prog", "pull", "--page_size", "20"]
+    with mock.patch("sys.argv", test_args):
+        args = setup_parser()
+        assert args.command == "pull"
+        assert args.page_size == 20
